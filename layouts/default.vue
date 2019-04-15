80b61bf2d238286d437a14aea2ad339775a7cde6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-15 11:27:37 +0900

    using NavBar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <nav-bar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import NavBar from '~/components/layouts/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
 
 <style>
 html {
